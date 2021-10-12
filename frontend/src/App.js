5fed91adf372b3fa991b4b8940a4318daab6c2fd
dominik24c <dominik24c@gmailc.com>
2021-10-12 23:31:42 +0200

    add react router dom and update navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Switch, Route} from 'react-router-dom';
 
 const App = ()=> {
   return (
@@ -9,6 +10,20 @@ const App = ()=> {
       {ReactDOM.createPortal(<Navbar/>,document.getElementById('navbar'))}
       {ReactDOM.createPortal(<Footer/>,document.getElementById('footer'))}
       <h2>Quiz app</h2>
+        <Switch>
+            <Route path="/" exact>
+                <h1>Homepage</h1>
+            </Route>
+            <Route path="/login">
+                <h1>Login</h1>
+            </Route>
+            <Route path="/sign-up">
+                <h1>Sing up</h1>
+            </Route>
+            <Route path="/logout">
+                <h1>Logout</h1>
+            </Route>
+        </Switch>
     </React.Fragment>
   );
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,27 @@
 import React from "react";
 import QuizIcon from '../quiz.png';
 import classes from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return(
         <React.Fragment>
             <div className={classes.brand}>
-                <a href="# ">
+                <NavLink to="/">
                     <img src={QuizIcon} alt="quiz icon"/>
-                </a>
+                </NavLink>
             </div>
             <nav>
                 <ul className={classes.links}>
-                    <li className={classes.link}><a href="#">Login</a></li>
-                    <li className={classes.link}><a href="#">Sign up</a></li>
-                    <li className={classes.link}><a href="#">Logout</a></li>
+                    <li className={classes.link}>
+                        <NavLink to="/login">Login</NavLink>
+                    </li>
+                    <li className={classes.link}>
+                        <NavLink to="/sign-up">Sign up</NavLink>
+                    </li>
+                    <li className={classes.link}>
+                        <NavLink to="/logout">Logout</NavLink>
+                    </li>
                 </ul>
             </nav>
         </React.Fragment>
